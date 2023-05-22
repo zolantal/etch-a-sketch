9837112a5ed0444d09ef0b269703c6e01577e10a
zolantal <zoltan_antal@outlook.com>
2023-05-22 16:34:40 +0200

    Add functionality to change grid size with slider

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ const noSquaresText = document.querySelector("#no-squares-text");
 noSquaresRange.value = noSquares;
 noSquaresText.value = noSquares;
 
-const squares = [];
+let squares = [];
 const squareBorderSize = 1;
-const squareSize = Math.floor(((gridSize - 2 * squareBorderSize * noSquares) / noSquares));
 const squareStartColour = "lightgrey";
 const squareEndColour = "black";
+
+const squareSize = Math.floor(((gridSize - 2 * squareBorderSize * noSquares) / noSquares));
 
 for (let i = 0; i < noSquares; i++) {
   const row = document.createElement("div");
@@ -66,3 +67,53 @@ function colorSquare(e) {
 //   noSquaresRange.value = val;
 // }
 
+noSquaresRange.addEventListener("input", noSquaresRangeChange);
+
+function noSquaresRangeChange(e) {
+  noSquaresText.value = e.target.value;
+  redrawGrid(e.target.value);
+}
+
+function redrawGrid(noSquares) {
+  while(grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  squares = [];
+
+  const squareSize = Math.floor(((gridSize - 2 * squareBorderSize * noSquares) / noSquares));
+
+  for (let i = 0; i < noSquares; i++) {
+    const row = document.createElement("div");
+
+    row.style.margin = 0;
+    row.style.padding = 0;
+    row.style.border = 0;
+    row.style.display = "flex";
+
+    for (let j = 0; j < noSquares; j++) {
+      const square = document.createElement("div");
+      square.setAttribute("id", i * noSquares + j)
+    
+      square.style.width = squareSize + "px";
+      square.style.height = squareSize + "px";
+      square.style.backgroundColor = squareStartColour;
+      square.style.margin = 0;
+      square.style.padding = 0;
+      square.style.border = squareBorderSize + "px";
+      square.style.borderStyle = "solid";
+      square.style.borderColor = "transparent";
+      square.style.backgroundClip = "padding-box";
+      
+      squares.push(square);
+      row.appendChild(square);
+    }
+
+    grid.appendChild(row);
+  }
+
+  squares.forEach(square => {
+    square.addEventListener("mouseenter", colorSquare);
+  }, {
+    once: true
+  });
+}
