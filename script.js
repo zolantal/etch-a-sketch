c828c46fc029d4fbaf56cfaeb22a7035cad797e3
zolantal <zoltan_antal@outlook.com>
2023-05-22 11:10:21 +0200

    Size grid div with JS instead

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const grid = document.querySelector("#grid");
+// const grid = document.getElementById("grid");
+const gridSize = 640;
+grid.style.width = gridSize + "px";
+grid.style.height = gridSize + "px";
+
+const squares = [];
+let size = 16;
+
+for (let i = 0; i < (size * size); i++) {
+  const square = document.createElement("div");
+
+  squares.push(square);
+}
+
 
